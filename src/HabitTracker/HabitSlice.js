ce60ce76261f596f81fe3d2a7168d847cdc1cc4c
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-05 14:55:21 +0100

    new commit

--- src/HabitTracker/HabitSlice.js
+++ src/HabitTracker/HabitSlice.js
@@ -1,39 +1,41 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
-
+import { createSlice, nanoid } from '@reduxjs/toolkit';
 
 
 const initialState = {
-  habits: JSON.parse(localStorage.getItem("habits")) || []
-}
+  habits: JSON.parse(localStorage.getItem('habits')) || [],
+};
+
+
+
 
 const habitSlice = createSlice({
-  name: "habits",
+  name: 'habits',
   initialState,
-  reducers:{
-    addHabits: (state, action) => {
+  reducers: {
+    addHabit: (state, action) => {
       state.habits.push({
         id: nanoid(),
         name: action.payload,
-        records: {}
-      })
-      localStorage.setItem("habits", JSON.stringify(state.habits))
+        records: {},
+      });
+      localStorage.setItem('habits', JSON.stringify(state.habits));
     },
-    toggleDay : (state, action) => {
-      const {habitId, date} = action.payload
-      const habit = state.habits.find((h)=> h.id === habitId)
-      if(habit) {
-        habit.records[date] = !habit.records[date]
-        localStorage.setItem("habits", JSON.stringify(state.habits))
+    toggleDay: (state, action) => {
+      const { habitId, date } = action.payload;
+      const habit = state.habits.find((h) => h.id === habitId);
+      if (habit) {
+        habit.records[date] = !habit.records[date];
+        localStorage.setItem('habits', JSON.stringify(state.habits));
       }
     },
-          deleteHabit: (state, action) => {
-            state.habits = state.habits.filter(h => h.id !== action.payload)
-            localStorage.setItem("habits", JSON.stringify(state.habits ))
-            
-          }
-  }
-})
+    deleteHabit: (state, action) => {
+      state.habits = state.habits.filter(h => h.id !== action.payload);
+      localStorage.setItem('habits', JSON.stringify(state.habits));
+    },
+  },
+});
 
-export const {addHabit, toggleDay, deleteHabit} = habitSlice.actions
-export default habitSlice.reducer
 
+export const { addHabit, toggleDay, deleteHabit } = habitSlice.actions;
+export default habitSlice.reducer;
+ 
