99570a318419b049c66a2b01e0c500c0fba2796d
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-06 21:37:47 +0100

    new commit

--- src/HabitTracker/HabitSlice.js
+++ src/HabitTracker/HabitSlice.js
@@ -33,7 +33,7 @@ const habitSlice = createSlice ({
 })
 
 export const { addHabit, toggledHabit, deleteHabit } = habitSlice.actions
-export default habitSlice.
+export default habitSlice.reducer
 
 
 
