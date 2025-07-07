9b212ddf3a73a932bf5508aeeb4cde1e83b01304
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-07 10:57:25 +0100

    new commit

--- src/HabitTracker/HabitSlice.js
+++ src/HabitTracker/HabitSlice.js
@@ -2,5 +2,13 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     habits: JSON.parse(localStorage.getItem('habits')) || [],
-    
+
 }
+
+const habitSlice = createSlice({
+    name: 'habits',
+    initialState,
+    reducers: {
+        
+    }
+})
