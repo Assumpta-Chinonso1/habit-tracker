9d21fd5b4def19b2475b76b79736538551393d91
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-08 13:50:51 +0100

    new commit

--- src/Components/HabitTracker.jsx
+++ src/Components/HabitTracker.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { addHabit } from "../HabitTracker/HabitSlice"
+import { format } from "date-fns"
+import { scale } from "framer-motion"
+import { motion } from "framer-motion"
 
 
 
@@ -32,7 +35,34 @@ return (
 
     <div className="habit-wrapper">
         <h1>My Habit Tracker</h1>
+        <div className="date-section">
+            <div>
+       WEEK OF {format(new Date(currentWeek[0]), 'MMM d')} - 
+         {format(new Date(currentWeek[6]),'MMM d')}
+            </div>
+            <div>
+                DATE {format(new Date(), 'MMM d, yyy')}
+            </div>
+        </div>
+        <div className="habit-input">
+            <input type="text"
+            value={habitName} 
+            placeholder="Enter Habit e.g Drink water"
+            onChange={(e)=> setHabitName(e.target.value)}/>
+            <motion.button
+             className= 'add-btn'
+             whileTap={{scale: 0.9}}
+              onClick={handleAdd} >
+                Add Habit
+            </motion.button>
+        </div>
 
+
+        <div className="calendar">
+            <div className="calendar-header">
+                <span className="header-title">Habit</span>
+            </div>
+        </div>
     </div>
     
 )
