c519eb2b37d0a516912777932f917fd2e44bc16a
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-08 14:39:56 +0100

    new commit

--- src/Components/HabitTracker.jsx
+++ src/Components/HabitTracker.jsx
@@ -1,1 +1,16 @@
 
+
+
+const HabitTracker = () => {
+  
+
+
+
+    return (
+        <div className="habit-wrapper">
+            <h1>My Habit Tracker</h1>
+        </div>
+    )
+}
+
+export default HabitTracker;
