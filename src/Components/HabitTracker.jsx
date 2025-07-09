465f6e1aefcd9e98fe8a435f8d7eec60ca0ec2cc
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-09 11:25:41 +0100

    New Commit

--- src/Components/HabitTracker.jsx
+++ src/Components/HabitTracker.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
 
 
 const HabitTracker = () => {
+const [habitName, setHabitName] = useState('')
+const habits = useSelector((state) => state.habits.habits)
+const dispatch = useDispatch()
+
 
 
 
