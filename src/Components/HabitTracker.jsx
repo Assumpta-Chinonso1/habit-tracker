6f229cd7cbb56ffe4c9f76d38019cfd3216cbfad
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-09 11:34:59 +0100

    New commit

--- src/Components/HabitTracker.jsx
+++ src/Components/HabitTracker.jsx
@@ -1,6 +1,7 @@
 import { setDate } from "date-fns"
 import { useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
+import { addHabit } from "../HabitTracker/HabitSlice"
 
 
 const HabitTracker = () => {
@@ -8,10 +9,19 @@ const [habitName, setHabitName] = useState('')
 const habits = useSelector((state) => state.habits.habits)
 const dispatch = useDispatch()
 
-const currentWeek = Array.from({length: 7}, (_, i))
-const today = new Date()
-today.setDate(today.getDate() - today.getDay() + i)
+const currentWeek = Array.from({length:7}, (_, i) => {
+    const today = new Date()
+    today.setDate(today.getDate() - today.getDay())
+    return today.toISOString().split('T')[0]
 
+})
+
+const handleAdd = (e) =>{
+    e.preventDefault()
+    if(habitName.trim()){
+        dispatch(addHabit.trim())
+    }
+}
 
 
     return(
