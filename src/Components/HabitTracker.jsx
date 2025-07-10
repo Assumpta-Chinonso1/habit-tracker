319befff12594269cac14812bc56353c665ea932
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-10 15:23:18 +0100

    New commit

--- src/Components/HabitTracker.jsx
+++ src/Components/HabitTracker.jsx
@@ -1,10 +1,19 @@
 import { useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
 
 
 const HabitTracker = () => {
-
-
+  
   const [habitName, setHabitName] = useState('')
+  const habits = useSelector((state) => state.habits.habits)
+  const dispatch = useDispatch()
+
+     const currentWeek = Array.from({length:7}, (_, i) =>{
+      const today = new Date()
+      today.setDate(today.getDate() - today.getDay() + i)
+     })
+
+     
 
 
 
