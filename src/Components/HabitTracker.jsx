f13278a513891c28f877251e3f981ef8868d1046
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-12 13:25:11 +0100

    new commit

--- src/Components/HabitTracker.jsx
+++ src/Components/HabitTracker.jsx
@@ -58,7 +58,7 @@ const  handleAdd = (e) => {
                className="header-day">
                 {format(new Date(date), "EEE")}
                 </span>
-                ))}
+               ))}
               </div>
 
               {habits.map((habit) => (
