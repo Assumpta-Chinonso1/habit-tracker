39c22c277fd0240bad4e512bc2207fd0f9bd0791
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-12 13:27:52 +0100

    new commit

--- src/Components/HabitTracker.jsx
+++ src/Components/HabitTracker.jsx
@@ -75,7 +75,7 @@ const  handleAdd = (e) => {
                     className={`day-box ${habit.records[date] ? "checked" : ''}`}
                     onClick={() => dispatch(toggleDay({habitId: habit.id, date}))}>
                        
-                      {habit.records[date] && <span className="checked">✅</span> }
+                     {habit.records[date] && <span className="checked">✅</span> }
                     </div>
                   ))}
                   <button className="delete-btn"
