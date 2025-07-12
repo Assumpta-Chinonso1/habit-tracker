d4079016e9c61587572c1d57116ee4b3a4c6862d
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-12 13:23:04 +0100

    new commit

--- src/Components/HabitTracker.jsx
+++ src/Components/HabitTracker.jsx
@@ -41,6 +41,7 @@ const  handleAdd = (e) => {
          placeholder="Enter Habit e.g Drink Water"
          onChange={(e) => setHabitName(e.target.value)} />
 
+
            <motion.button
            className="add-btn"
            whileTap={{scale:0.9}}
