9d12ee7450b2f8308bf17c6565e045261335fa94
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-15 16:41:45 +0100

    new commit

--- src/HabitTracker/HabitSlice.js
+++ src/HabitTracker/HabitSlice.js
@@ -26,7 +26,7 @@
                 localStorage.setItem('habits', JSON.stringify(state.habits))
              }
           },
-           deleteHabit:  (state, action) => {
+        deleteHabit:  (state, action) => {
           state.habits = state.habits.filter((h)=>h.id !== action.payload)
      }
     }
