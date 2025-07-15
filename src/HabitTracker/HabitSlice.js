4f7a10fdb25c6acd5a836df012a773a8e3285d3d
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-15 16:36:58 +0100

    new commit

--- src/HabitTracker/HabitSlice.js
+++ src/HabitTracker/HabitSlice.js
@@ -25,7 +25,7 @@ import { createSlice, nanoid } from "@reduxjs/toolkit";
                 habit.records[date] = !habit.records[date]
                 localStorage.setItem('habits', JSON.stringify(state.habits))
              }
-         },
+          },
          deleteHabit:  (state, action) => {
           state.habits = state.habits.filter((h)=>h.id !== action.payload)
         }
