260b036f208916ef854bf84cf8e46b530c88ad0f
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-15 16:37:20 +0100

    new commit

--- src/HabitTracker/HabitSlice.js
+++ src/HabitTracker/HabitSlice.js
@@ -16,7 +16,7 @@ import { createSlice, nanoid } from "@reduxjs/toolkit";
                 records: {}
           })
            localStorage.setItem('habits', JSON.stringify(state.habits))
-         },
+          },
             toggleDay: (state, action ) =>{
             const {habitId, date} = action.payload
             const habit = state.habits.find((h)=> h.id === habitId)
